eb6c2fdbaa86b8d13c71d7b0e434ccc1e3951746
Reivaxy <reivaxy@users.noreply.github.com>
2022-05-30 15:44:14 +0200

    No longer using HTTPClient class: solved wdt reboot !

--- src/XIOTHttps.cpp
+++ src/XIOTHttps.cpp
@@ -6,11 +6,13 @@
  */
 
 #include "XIOTHttps.h"
+#include <XUtils.h>
 
 int XIOTHttps::sendToHttps(const char* method, const char* url, const char* payload) {
-   return sendToHttps(method, url, payload, 1024, 15000);
+   return sendToHttps(method, url, payload, 2048, 3000);
 }
 
+// Only for PUT and POST for now.
 int XIOTHttps::sendToHttps(const char* method, const char* url, const char* payload, int bufferSize, int timeout) {
   MemSize("XIOTHttps::sendToHttps");
   int httpCode = -1;
@@ -21,30 +23,58 @@ int XIOTHttps::sendToHttps(const char* method, const char* url, const char* payl
   // use small buffers since payloads are small
   // Otherwise it will use almost 30K of ram and sometimes crash :(
   client.setBufferSizes(bufferSize, bufferSize);  
-  HTTPClient https;
-  if (https.begin(client, url)) {
-    yield(); // attempt to lower WDT risk ? lol
-    // start connection and send  headers
-    https.addHeader("Content-Type", "application/json");
-    if (strncmp("POST", method, 4) == 0) {
-      Debug("POST\n");
-      httpCode = https.POST((const uint8_t *)payload, strlen(payload));
-    } else {
-      Debug("PUT\n");
-      httpCode = https.PUT((const uint8_t *)payload, strlen(payload));
-    }
-    MemSize("after sending https request");
-    Debug("HTTP response code: %d\n", httpCode);
-    if (httpCode > 200) {
-      Serial.println(https.getString().c_str());
-    }
-    if (httpCode < 0) {
-      Serial.println(https.errorToString(httpCode).c_str());
-    }
 
-    https.end();
+  char *urlCopy = XUtils::mallocAndCopy(url);
+  char *firstPathSlash = strchr(urlCopy + 9, '/');
+  *firstPathSlash = 0;
+  char *host = urlCopy + 8;
+
+  // Serial.printf("Url: %s\n", url);
+  // Serial.printf("Host: %s\n", host);
+
+  if (!client.connect(host, 443)) {
+    Serial.println("Connection failed!");
   } else {
-    Serial.println("Connection failed\n");
+    client.printf("%s %s HTTP/1.1\n", method, url);
+    client.printf("Host: %s\n", host);
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.printf("Content-Length: %d\n", strlen(payload));
+    client.println();  
+    client.println(payload);
+
+    // read output
+    // First line is result:
+    // HTTP/1.1 401 Unauthorized
+    // HTTP/1.1 200 OK
+    // Next lines are response headers until empty line
+    Debug("Response:\n");
+    bool firstLine = true;
+
+    // TODO: handle timeout ?
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (firstLine) {
+        httpCode = line.substring(9, 12).toInt();
+        firstLine = false;
+      }
+      Debug("%s\n", line.c_str());
+      if (line == "\r") {
+        break;
+      }
+    }
+    // Don't know if reading them is mandatory if we don't need them?
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available()) {
+      char c = client.read();
+      //Serial.write(c);
+    }
+    client.stop();
+    Serial.printf("HTTP response code %d\n", httpCode);
+
   }
+
+  free(urlCopy);
   return httpCode;
 }
